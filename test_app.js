35a1c363e9b16ddca68931e51e6915b7264b0e63
Paul Andrei <paulandreitaranu@gmail.com>
2020-07-11 15:11:54 +0100

    Falling Down

--- test_app.js
+++ test_app.js
@@ -1,45 +1,46 @@
+// 2flMaRq183mq7OXb
 const path = require("path");
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 const https = require("https");
-
 const express = require("express");
 const bodyParser = require("body-parser");
+const multer = require("multer");
 const mongoose = require("mongoose");
 const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
+const MongoDbStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
 const flash = require("connect-flash");
-const multer = require("multer");
 const helmet = require("helmet");
 const compression = require("compression");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 
+// ROUTES & CONTROLLERS
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 const errorController = require("./controllers/error");
-const shopController = require("./controllers/shop");
-const isAuth = require("./middleware/is-auth");
-const User = require("./models/user");
+// const accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), { flags: "a" });
 
+// DATABASES & MODELS
+const User = require("./models/user");
 const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.1l8tr.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`;
 
-const app = express();
-const store = new MongoDBStore({
+const store = new MongoDbStore({
     uri: MONGODB_URI,
     collection: "sessions",
 });
-const csrfProtection = csrf();
-
-// const privateKey = fs.readFileSync('server.key');
-// const certificate = fs.readFileSync('server.cert');
-
+const csurfProtection = csrf();
+// const privateKey = fs.readFileSync("server.key");
+// const certificate = fs.readFileSync("server.cert");
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "images");
     },
     filename: (req, file, cb) => {
-        cb(null, new Date().toISOString() + "-" + file.originalname);
+        cb(null, uuidv4() + "-" + file.originalname);
     },
 });
-
 const fileFilter = (req, file, cb) => {
     if (file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg") {
         cb(null, true);
@@ -48,41 +49,29 @@ const fileFilter = (req, file, cb) => {
     }
 };
 
+// APP
+const app = express();
+app.use(session({ secret: "mySecret", resave: false, saveUninitialized: false, store: store }));
+
+// app.use(morgan("combined", { stream: accessLogStream }));
+
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const authRoutes = require("./routes/auth");
-
-const accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), { flags: "a" });
-
 app.use(helmet());
 app.use(compression());
-app.use(morgan("combined", { stream: accessLogStream }));
-
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(csurfProtection);
 app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single("image"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "images")));
-app.use(
-    session({
-        secret: "my secret",
-        resave: false,
-        saveUninitialized: false,
-        store: store,
-    })
-);
-
 app.use(flash());
-
 app.use((req, res, next) => {
     res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
     next();
 });
-
 app.use((req, res, next) => {
-    // throw new Error('Sync Dummy');
     if (!req.session.user) {
         return next();
     }
@@ -99,25 +88,12 @@ app.use((req, res, next) => {
         });
 });
 
-app.post("/create-order", isAuth, shopController.postOrder);
-
-app.use(csrfProtection);
-app.use((req, res, next) => {
-    res.locals.csrfToken = req.csrfToken();
-    next();
-});
-
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
-
 app.get("/500", errorController.get500);
-
 app.use(errorController.get404);
-
 app.use((error, req, res, next) => {
-    // res.status(error.httpStatusCode).render(...);
-    // res.redirect('/500');
     res.status(500).render("500", {
         pageTitle: "Error!",
         path: "/500",
@@ -127,11 +103,12 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-    .connect(MONGODB_URI)
+    .connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then((result) => {
-        // https
-        //   .createServer({ key: privateKey, cert: certificate }, app)
-        //   .listen(process.env.PORT || 3000);
+        // https.createServer({ key: privateKey, cert: certificate }, app).listen(process.env.PORT || 3000);
         app.listen(process.env.PORT || 3000);
     })
     .catch((err) => {
